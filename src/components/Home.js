e96b01d3e462300b3961d0a214819664f55878b2
Afzal Ahmed <52744257+afzalahmed81@users.noreply.github.com>
2020-07-15 21:59:14 +0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import React from 'react';
 export const Home= () => {
     return (
         <div>
+            <br /> <br /><br /><br />
             <h1>Welcome to the Online Shoe Store </h1>
         </div>
     )
